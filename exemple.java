ca579a43f8b79449550bba7f6bc91eadf5fadadb
El vostre nom <usuari@domini.com>
2020-11-05 10:12:09 +0100

    VersionNova V.2

--- exemple.java
+++ exemple.java
@@ -14,6 +14,8 @@ public class exemple {
 			}
 		} else {
 			System.out.println("\033[34m" + "Del 1-10 porfavor, cara alcachofa");
+			usrNum = scanner.nextInt();
+			System.out.println("\033[35" + "Mmmm, nice papita")
 		}
 	}
 }
