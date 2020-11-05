8d3025b269e03ff7e83c3802fd33ec22d87ff4ab
El vostre nom <usuari@domini.com>
2020-11-05 10:19:08 +0100

    Cambié el nombre porque puse exemple cuando tenia que ser IF xddd

---
+++ If.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class If {
+	public static void main (String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("\033[31m" + "Introduce un numero" + "\u001B[0m");
+		//Ask for a number
+		int usrNum = scanner.nextInt();
+
+		if(usrNum >= 0 && usrNum <=10){
+			for (int i = 0; i <= 10; i++) {
+				int multiply = (i * usrNum);
+				System.out.println("\033[32m" + usrNum + " x " + i + " = " + multiply + "\u001B[0m");
+			}
+		} else {
+			System.out.println("\033[34m" + "Del 1-10 porfavor, cara alcachofa");
+			usrNum = scanner.nextInt();
+			System.out.println("\033[35" + "Mmmm, nice papita");
+		}
+	}
+}
